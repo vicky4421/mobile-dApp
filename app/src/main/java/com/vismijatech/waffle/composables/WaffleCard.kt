bfbfe1f6de76954d45ccf74047e10ad2aa6fe6ee
Vivek Sawant <vicky210490@gmail.com>
2024-10-05 14:21:36 +0530

    second module

---
+++ app/src/main/java/com/vismijatech/waffle/composables/WaffleCard.kt
@@ -0,0 +1,6 @@
+package com.vismijatech.waffle.composables
+
+/**
+* Created by Vivek Sawant on 04-10-2024.
+*/class WaffleCard {
+}
---
+++ app/src/main/java/com/vismijatech/waffle/injection/RpcConfig.kt
@@ -0,0 +1,6 @@
+package com.vismijatech.waffle.injection
+
+/**
+* Created by Vivek Sawant on 04-10-2024.
+*/class RpcConfig {
+}
